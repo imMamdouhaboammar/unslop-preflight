603efa8c52d299e6ec812baf07c98fe85961dc5c
mamdouh aboammar <58908124+imMamdouhaboammar@users.noreply.github.com>
2026-06-30 12:44:03 +0300

    Add initial source slop detectors

--- src/scanners/sourceSlopScanner.js
+++ src/scanners/sourceSlopScanner.js
@@ -1,1 +1,17 @@
-export const sourceSlopRules = [];
+export const sourceSlopRules = [
+  {
+    name: 'unstable-random-key',
+    level: 'blocker',
+    pattern: /key=\{?\s*(Math\.random\(\)|Date\.now\(\))/i
+  },
+  {
+    name: 'array-index-key-reorder-risk',
+    level: 'warning',
+    pattern: /key=\{\s*(index|idx|i)\s*\}|key=["']\s*(index|idx|i)\s*["']/i
+  },
+  {
+    name: 'transition-all-animation-slop',
+    level: 'info',
+    pattern: /\btransition-all\b/i
+  }
+];
